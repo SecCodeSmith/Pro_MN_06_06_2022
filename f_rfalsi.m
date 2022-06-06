97b5169c42a7761abef1a5b8138432aebd9a46b9
kubab16 <kubabb161@gmail.com>
2022-06-06 19:44:09 +0200

    drugi commit

--- f_rfalsi.m
+++ f_rfalsi.m
@@ -10,16 +10,16 @@ if (sign(ya) == sign(yb))
 	error('Funkcja nie ma r�nych znak�w na ko�cach przedzia�u.')
 end
 
-disp('Krok a  b c yc  blad') ;
+disp('Krok a  b c yc  blad blod_rzczywisty') ;
 
 for ii=1:1:k_max
   e = e / 2;
 	c = b - (b - a) / (yb - ya) * yb;
   yc = eval([fun '(c)']) ; % fun1(c) ;
 
-  blad = (1 / 2^ii) .* (b - a);
-
- 	out = [ii a b c yc blad]; disp(out);
+  blad = (1 / 2^ii) * (b - a);
+  blad_rzeczywisty = 5.60979 - c;
+ 	out = [ii a b c yc blad blad_rzeczywisty]; disp(out);
  	OUT(ii,:) = out ;
 
   if (abs(e) < tol || abs(yc) < tol)
